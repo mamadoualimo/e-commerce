4e2f603a8d74f5bb0f5549e0e9ecda39824ba8fb
mamadoualimo <58432140+mamadoualimo@users.noreply.github.com>
2020-06-20 22:29:51 +0200

    fonctionnalites et données des produits

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,7 +19,7 @@ export default class Navbar extends Component {
                 <ul className="navbar-nav align-items-center">
                     <li className="nav-item ml-5">
                         <Link to="/" className="nav-link">
-                            products
+                            produits
                         </Link>
                     </li>
                 </ul>
@@ -28,7 +28,7 @@ export default class Navbar extends Component {
                         <span>
                             <i className="fas fa-cart-plus" />
                         </span>
-                        my cart
+                        panier
                     </ButtonContainer>
                 </Link>
             </NavWrapper>
